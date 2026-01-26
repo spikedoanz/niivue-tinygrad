20803d5d401b6b74a7f7e458879acae3b228fa82
spike <spikedoanz@gmail.com>
2026-01-26 12:36:37 -0500

    lazycommit

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import subcortical_tta_sagittal from "./net_subcortical_tta_sagittal.js"
 import subcortical_tta_coronal from "./net_subcortical_tta_coronal.js"
 import subcortical_tta_axial from "./net_subcortical_tta_axial.js"
 import tissue_fast from "./net_tissue_fast.js"
+import tissue_fast_32MB from "./net_tissue_fast_32MB.js"
 import tissue_fast_64MB from "./net_tissue_fast_64MB.js"
 import tissue_fast_128MB from "./net_tissue_fast_128MB.js"
 import tissue_fast_256MB from "./net_tissue_fast_256MB.js"
@@ -68,6 +69,13 @@ const models = {
   "tissue_fast_tta_all": {
     "net": tissue_fast_tta_all,
     "weightPath": "./net_tissue_fast_tta_all.safetensors",
+    "colormap": "./colormap_tissue_subcortical.json",
+    "volume": "./t1_crop.nii.gz",
+    "normalization": "min-max"
+  },
+  "tissue_fast_32MB": {
+    "net": tissue_fast_32MB,
+    "weightPath": "./net_tissue_fast_32MB.safetensors",
     "colormap": "./colormap_tissue_subcortical.json",
     "volume": "./t1_crop.nii.gz",
     "normalization": "min-max"
